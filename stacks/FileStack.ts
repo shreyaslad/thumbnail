12119f25b4cd35fa2f6beb92af0ea0feddecd4e2
Shreyas Lad <slad0716@gmail.com>
2023-03-13 04:00:40 -0700

    fix: Refactor tab size to 2 spaces

--- stacks/FileStack.ts
+++ stacks/FileStack.ts
@@ -3,9 +3,9 @@ import { StackContext, Bucket } from "sst/constructs";
 export function FileStack({ stack }: StackContext) {
   const raw_videos_bucket = new Bucket(stack, "raw-upload-bucket", {
     cdk: {
-        bucket: {
-            bucketName: `${stack.stage}-thumbnail-raw-videos`
-        }
+      bucket: {
+        bucketName: `${stack.stage}-thumbnail-raw-videos`
+      }
     }
   });
   
